44c52fea994246717d67d2a32e0812a75b4348cb
allitov <litovkinalexan@gmail.com>
2024-01-25 16:59:02 +0700

    Add Dockerfile and docker-compose

---
+++ src/main/resources/db/demo_data.sql
@@ -0,0 +1,11 @@
+insert into categories (name) values ('Thriller');
+insert into categories (name) values ('Horror');
+insert into categories (name) values ('Comedy');
+insert into categories (name) values ('Children');
+insert into categories (name) values ('Western');
+
+insert into books (name, author, category_id, publication_date) values ('Song of the Bloodred Flower (Laulu tulipunaisesta kukasta)', 'Nels Lamerton', 1, '2012-06-20');
+insert into books (name, author, category_id, publication_date) values ('Pin...', 'Oralle Tarbin', 3, '2005-05-25');
+insert into books (name, author, category_id, publication_date) values ('1612: Khroniki smutnogo vremeni', 'Adlai Leader', 5, '2008-10-07');
+insert into books (name, author, category_id, publication_date) values ('Travelling Players, The (O thiasos)', 'Adelheid Goscomb', 4, '2007-04-28');
+insert into books (name, author, category_id, publication_date) values ('Prisoners of the Lost Universe', 'Sisile Erricker', 4, '2009-12-06');
