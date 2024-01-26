b1b7d130aef53533ea55a728726787e15de25909
allitov <litovkinalexan@gmail.com>
2024-01-26 19:28:09 +0700

    Fix book repository

--- src/main/java/com/allitov/booksapi/model/repository/BookRepository.java
+++ src/main/java/com/allitov/booksapi/model/repository/BookRepository.java
@@ -2,12 +2,11 @@ package com.allitov.booksapi.model.repository;
 
 import com.allitov.booksapi.model.data.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findFirstBookByNameAndAuthor(String bookName, String author);
 
