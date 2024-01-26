19a792407ae863075c90c3fa3b004dc0db3b443d
allitov <litovkinalexan@gmail.com>
2024-01-26 19:55:23 +0700

    Remove unnecessary import in book repository

--- src/main/java/com/allitov/booksapi/model/repository/BookRepository.java
+++ src/main/java/com/allitov/booksapi/model/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.allitov.booksapi.model.repository;
 
 import com.allitov.booksapi.model.data.Book;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
